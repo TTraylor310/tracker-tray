525716c60e6913cd6f0e98545c32c05a68ca8723
Tim Traylor <ttraylor83@gmail.com>
2023-01-31 02:53:45 +0000

    header intro to application

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { toast, ToastContainer } from 'react-toastify'
+import Header from './components/Header'
 
 function App() {
+
+  const notify = () => toast('Wow, so Easy!!!')
+
   return (
     <>
-      <h1>App home page</h1>
+      <Router>
+        <div className="container">
+          <Header />
+        </div>
+      </Router>
+      <ToastContainer />
     </>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="logo">
+        Support Desk
+      </div>
+      <ul>
+        <li>Login</li>
+        <li>Register</li>
+      </ul>
+    </header>
+  )
+}
+export default Header
